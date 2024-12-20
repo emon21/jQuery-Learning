c3d3700c24005d2add1d8a98e6611afeb9628acd
emon21 <emonbd66@gmail.com>
2024-12-20 20:32:42 +0600

    Done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 // Custom JS Code
 
-// JS Code
-
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("Hello World");
-});
-
 
 //Jquery Code
-// $(document).ready(function() {
-//    console.log("Hello World");
-// });
+$(document).ready(function() {
+   console.log("Hello World");
+});
