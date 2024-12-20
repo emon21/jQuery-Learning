7cbd7194860199d8f241d73981e836325112bfc1
emon21 <emonbd66@gmail.com>
2024-12-20 20:03:46 +0600

    feat:Jquery Lerning Project Setup

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Custom JS Code
+
+// JS Code
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Hello World");
+});
+
+
+//Jquery Code
+// $(document).ready(function() {
+//    console.log("Hello World");
+// });
